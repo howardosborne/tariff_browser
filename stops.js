89d3b46fd52d1ccd0280f27481b11dd977c11b52
howardosborne <hosborne@hotmail.com>
2024-05-10 22:57:01 +0100

    stuff

--- stops.js
+++ stops.js
@@ -1,37 +1,78 @@
 var map;
-var stopmarkers;
+var origins;
+var destinations;
 var stats;
-var transferTime = 600;
-
+var terTariffs = {};
+var stops = {};
 function setup(){
 	map = L.map('map').setView([45, 10], 5);
 	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19,attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
-    stopmarkers = L.markerClusterGroup({maxClusterRadius:40});
-	map.addLayer(stopmarkers);
+    origins = L.markerClusterGroup({maxClusterRadius:40});
+	map.addLayer(origins);
+    destinations = L.markerClusterGroup({maxClusterRadius:40});
+	map.addLayer(destinations);
 	getStops();
+	getTarrifs();
+ }
+
+ async function getTarrifs(){
+	const url= `././tarifs-ter-par-od.json`;
+	const response = await fetch(url);
+	terTariffs = await response.json();
  }
 
  async function getStops(){
 	const url= `./referentiel-gares-voyageurs.json`;
 	const response = await fetch(url);
-	const stops = await response.json();
+	const stopsjson = await response.json();
 	
-    Object.entries(stops).forEach((entry) => {
-		const [id, stop] = entry;
+    Object.entries(stopsjson).forEach((entry) => {
+		let [id, stop] = entry;
 		//{"code": "02027-2", "code_gare": "02027", "uic_code": "0087733667", "dtfinval": null, "alias_libelle_noncontraint": "Mareil-Marly T13", "adresse_cp": "78750", "commune_code": "367", "commune_libellemin": "Mareil-Marly", "departement_numero": "78", "departement_libellemin": "Yvelines", "longitude_entreeprincipale_wgs84": null, "latitude_entreeprincipale_wgs84": null, "segmentdrg_libelle": null, "niveauservice_libelle": null, "rg_libelle": "Gares C Ile-de-France", "gare_alias_libelle_noncontraint": "Mareil-Marly", "gare_alias_libelle_fronton": "Mareil-Marly", "gare_agencegc_libelle": "DGIF", "gare_regionsncf_libelle": "REGION DE PARIS SAINT-LAZARE", "gare_ug_libelle": null, "gare_ut_libelle": "MAREIL MARLY GARE", "gare_nbpltf": 2, "tvs": null, "wgs_84": null}
 		if (stop.longitude_entreeprincipale_wgs84){
+			stop.uic8 = parseInt(stop.uic_code).toString();
+			stops[stop.uic8] = stop;
 			let my_icon = L.icon({iconUrl: `./departure_board.png`,iconSize: [24, 24], iconAnchor: [12,24]});
 			let marker = L.marker([stop.latitude_entreeprincipale_wgs84, stop.longitude_entreeprincipale_wgs84],{icon:my_icon});
 			marker.bindTooltip(decodeURI(stop.gare_ut_libelle));
 			marker.properties = stop;
 			marker.addEventListener('click', _showConnections);
-			marker.addTo(stopmarkers);
+			marker.addTo(origins);
 		}
 	  });
   }
 
 async function _showConnections(e){
-	L.popup().setLatLng([e.latlng.lat,e.latlng.lng]).setContent(e.sourceTarget.properties.gare_ut_libelle).openOn(map); 
+	destinations.clearLayers();
+	//origins.clearLayers();
+	document.getElementById("details").innerHTML = "";
+	document.getElementById("details").insertAdjacentHTML('beforeend',`<h5>${e.sourceTarget.properties.gare_ut_libelle}</h5>`);	
+    Object.entries(terTariffs).forEach((entry) => {
+		const [id, tariff] = entry;
+		if(tariff.origine_uic == e.sourceTarget.properties.uic8 && tariff.type_tarif=="Tarif normal"){
+			let dest = stops[tariff.destination_uic]; 
+			element = `
+				<div class="card">
+				<div class="card-body">
+				<ul class="list-group list-group-flush">
+				<li class="list-group-item">${tariff.destination}</li>
+				<li class="list-group-item">${tariff.prix_eur}</li>
+				<li class="list-group-item">${tariff.type_tarif}</li>
+				</ul>
+				</div>
+				</div>`;
+			document.getElementById("details").insertAdjacentHTML('beforeend',element);
+
+			let my_icon = L.icon({iconUrl: `./hop.png`,iconSize: [24, 24], iconAnchor: [12,24]});
+			let marker = L.marker([dest.latitude_entreeprincipale_wgs84, dest.longitude_entreeprincipale_wgs84],{icon:my_icon});
+			marker.properties = tariff;
+			marker.addEventListener('click', _showDetails);
+			marker.addTo(destinations);
+		}
+	});
+	var of = document.getElementById("offcanvasDetails");
+	var offcanvas = new bootstrap.Offcanvas(of);
+	offcanvas.toggle();
 }
 
 function formatTime(timestamp){
@@ -44,58 +85,13 @@ function formatTime(timestamp){
 
 }
 
-function showDetails(){
-	document.getElementById("details").innerHTML = "";
-	stats.details.forEach(item=>{
-		if(item.status =="missed connection"){
-			element = `
-			<div class="card">
-				<div class="card-header">${formatTime(item.arrival.when)} from ${item.arrival.provenance} ${item.arrival.delay/60} minutes late <span class="badge text-bg-danger">late</span>
-				</div>
-				<div class="card-body">
-					<ul class="list-group list-group-flush">
-					missed connection to ${item.departure.direction} 
-					</ul>
-					<ul class="list-group list-group-flush">
-					Missed departure to ${item.departure.direction} at ${formatTime(item.departure.when)}
-					</ul>
-				</div>
-			</div>	`;
-			document.getElementById("details").insertAdjacentHTML('beforeend',element);
-		}
-		else if(item.status =="connection at risk"){
-			element = `
-			<div class="card">
-				<div class="card-header">${formatTime(item.arrival.when)} from ${item.arrival.provenance} ${item.arrival.delay/60} minutes late <span class="badge text-bg-warning">late</span>
-				</div>
-				<div class="card-body">
-				<ul class="list-group list-group-flush">
-				Short interchange time for connection to ${item.departure.direction} at ${formatTime(item.departure.when)}
-				</ul>
-				</div>
-			</div>`;
-			document.getElementById("details").insertAdjacentHTML('beforeend',element);
-		}
-		else if(item.status =="arrival on time"){
-		element = `
-		<div class="card">
-			<div class="card-header">${formatTime(item.arrival.when)} from ${item.arrival.provenance} <span class="badge text-bg-success">on time</span>
-			</div>
-		</div>`;
-		document.getElementById("details").insertAdjacentHTML('beforeend',element);
-		}
-		else{
-			element = `
-			<div class="card">
-				<div class="card-header">arrival from ${item.arrival.provenance}
-				</div>
-			</div>`;
-			document.getElementById("details").insertAdjacentHTML('beforeend',element);			
-		}
-	})
-	var of = document.getElementById("offcanvasDetails");
-	var offcanvas = new bootstrap.Offcanvas(of);
-	offcanvas.toggle();
+function _showDetails(e){
+	let tariff = e.sourceTarget.properties;
+	let popupText = `<ul class="list-group list-group-flush">
+		<li class="list-group-item">${tariff.destination}</li>
+		<li class="list-group-item">${tariff.prix_eur}</li>
+		</ul>`;
+	L.popup().setLatLng([e.latlng.lat,e.latlng.lng]).setContent(popupText).openOn(map);	
 }
 
 function setupStops(){
